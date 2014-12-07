ec4b1651b504be1d8a240c01c89c0b28f6ba4007
Ori Hoch <ori.hoch@kaltura.com>
2014-12-07 18:28:23 +0200

    update test

--- lobbyists/tests/search-companies.casper.js
+++ lobbyists/tests/search-companies.casper.js
@@ -3,17 +3,15 @@ casper.test.begin('searching lobbyist represents', 2, function suite(test) {
     casper
         .start('http://localhost:8000/lobbyist/124')
         .then(function() {
-            test.assert(casper.getElementsInfo('section.lobbyist-represents li').length > 1)
+            test.assert(casper.getElementsInfo('section.lobbyist-represents li:not(.hidden)').length > 1)
         })
         .then(function(){
-            casper.fillSelectors('#content-main', {
-                '#lobbyist-company-search input': 'WJ Technologies'
-            });
+            casper.sendKeys('#lobbyist-company-search input', 'WJ Technologies');
         })
         .waitFor(function(){
-            return casper.getElementsInfo('section.lobbyist-represents li').length == 1;
+            return casper.getElementsInfo('section.lobbyist-represents li:not(.hidden)').length == 1;
         }, function() {
-            test.assertSelectorHasText('section.lobbyist-represents li', 'WJ Technologies');
+            test.assertSelectorHasText('section.lobbyist-represents li:not(.hidden)', 'WJ Technologies');
         }, function() {
             test.fail('did not get a single li element for the lobbyist represents')
         })
